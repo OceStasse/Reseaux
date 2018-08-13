a3d6d7478f5564efd4dc26313969c8fd9b3d94ce
rouffou <rouffou@gmail.com>
2018-08-14 01:11:02 +0200

    ...

--- LaboReseauxBinome/TestProject/src/testproject/TestProject.java
+++ LaboReseauxBinome/TestProject/src/testproject/TestProject.java
@@ -2,10 +2,14 @@ package testproject;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import javax.swing.JPasswordField;
 
 public class TestProject {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
-        System.out.println("12345".subSequence(0, 2));
+        JPasswordField pwd = new JPasswordField("test");
+        System.err.println(String.valueOf(pwd.getPassword()));
+     
+        
     }
 }
