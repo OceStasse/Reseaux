a89a099e6e7b19564d048e659b7ccc82617ce21c
rouffou <rouffou@gmail.com>
2018-09-04 18:55:29 +0200

    .....

---
+++ final eclipse/Librairie/src/network/protocole/tickmap/reponse/ReponsePassenger.java
@@ -0,0 +1,17 @@
+package network.protocole.tickmap.reponse;
+
+import generic.server.AReponse;
+
+public class ReponsePassenger extends AReponse {
+
+	protected ReponsePassenger(String message, boolean successful) {
+		super(message, successful);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
